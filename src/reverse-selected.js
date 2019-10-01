4e7380868c2857667e0e0bf8acb4cd2348cf6d0c
Daniel White <danielwhite@Daniels-MacBook-Pro.local>
2019-09-30 19:17:08 -0500

    add reverse layers

---
+++ src/reverse-selected.js
@@ -0,0 +1,14 @@
+const sketch = require('sketch');
+const document = sketch.getSelectedDocument();
+const selectedLayers = document.selectedLayers.layers;
+
+var indexArray = new Array();
+selectedLayers.forEach(nativeLayer => {
+  var layer = sketch.fromNative(nativeLayer);
+  indexArray.push(layer.index);
+});
+
+selectedLayers.forEach(nativeLayer => {
+  var layer = sketch.fromNative(nativeLayer);
+  layer.index = indexArray.pop();
+});
